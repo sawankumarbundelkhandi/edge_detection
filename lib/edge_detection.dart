6f7e6f9aa65392d7ab21d547d964d6791cefc56c
espbee <elliotpo@icloud.com>
2021-05-11 14:54:20 -0400

    null safe changes in edge_detection.dart

--- lib/edge_detection.dart
+++ lib/edge_detection.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 class EdgeDetection {
   static const MethodChannel _channel = const MethodChannel('edge_detection');
 
-  static Future<String> get detectEdge async {
-    final String version = await _channel.invokeMethod('edge_detect');
+  static Future<String?> get detectEdge async {
+    final String? version = await _channel.invokeMethod('edge_detect');
     return version;
   }
 }
