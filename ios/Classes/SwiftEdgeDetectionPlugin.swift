757ea5a1e3a4189364574ca36177327fb2ed86ce
Sawan Kumar Bundelkhandi <sawankumarbundelkhandi@gmail.com>
2021-05-14 17:24:26 +0530

    format swift code.

--- ios/Classes/SwiftEdgeDetectionPlugin.swift
+++ ios/Classes/SwiftEdgeDetectionPlugin.swift
@@ -3,20 +3,24 @@ import UIKit
 import WeScan
 
 public class SwiftEdgeDetectionPlugin: NSObject, FlutterPlugin, UIApplicationDelegate {
-  
-    
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "edge_detection", binaryMessenger: registrar.messenger())
-    let instance = SwiftEdgeDetectionPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-    registrar.addApplicationDelegate(instance)
-  }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if let viewController = UIApplication.shared.delegate?.window??.rootViewController as? FlutterViewController {
-        let destinationViewController = HomeViewController()
-        destinationViewController._result = result
-        viewController.present(destinationViewController,animated: true,completion: nil);
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "edge_detection", binaryMessenger: registrar.messenger())
+        let instance = SwiftEdgeDetectionPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        registrar.addApplicationDelegate(instance)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+      
+        if (call.method == "edge_detect")
+        {
+            if let viewController = UIApplication.shared.delegate?.window??.rootViewController as? FlutterViewController {
+                let destinationViewController = HomeViewController()
+                destinationViewController._result = result
+                viewController.present(destinationViewController,animated: true,completion: nil);
+            }
+        }
+    }
+
 }
