9db7fa7a43ded60df3f2abec50c059b54718eba1
GamalielCrdz <programacion@arema.mx>
2021-07-16 13:38:17 -0500

    cambios
    Signed-off-by: GamalielCrdz <programacion@arema.mx>

--- android/src/main/kotlin/com/sample/edgedetection/scan/ScanPresenter.kt
+++ android/src/main/kotlin/com/sample/edgedetection/scan/ScanPresenter.kt
@@ -237,6 +237,11 @@ class ScanPresenter constructor(private val context: Context, private val iView:
                         val corner = processPicture(img)
                         busy = false
                         if (null != corner && corner.corners.size == 4) {
+                            val heigth: Double = distance(corner)
+                            if (heigth < 170 ) {
+                                Log.i(TAG, "------ $heigth heigth-------------")
+                                it.onError(Throwable("to small"))
+                            }
                             it.onNext(corner)
                         } else {
                             it.onError(Throwable("paper not detected"))
@@ -253,6 +258,17 @@ class ScanPresenter constructor(private val context: Context, private val iView:
             print(e.message)
         }
 
+    }
+
+    private fun distance(corners: Corners): Double {
+        val tl = corners.corners[0] ?: org.opencv.core.Point()
+        val bl = corners.corners[3] ?: org.opencv.core.Point()
+        
+        val d : Double = sqrt((
+            (tl.x.toFloat()-bl.x.toFloat()).pow(2) + 
+            (tl.y.toFloat()-bl.y.toFloat()).pow(2)
+            ).toDouble())
+        return d
     }
 
     private fun getMaxResolution(): Camera.Size? =
